a0cd846e990978c5201194dd6abf068a1c179588
omesh2006 <131674926+omesh2006@users.noreply.github.com>
2023-05-12 11:12:21 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>oke</title>
+</head>
+<body>
+<?php
+
+$klein = 10;
+$groot = 12;
+
+$antwoord = $klein + $groot;
+echo $klein . "+" . $groot . "=" . $antwoord.     
+"<br>";
+
+$laaggetal = 10;
+$hooggetal = 12;
+
+$antwoord2 = $laaggetal + $hooggetal;
+
+echo $laaggetal . "*" . $hooggetal . "=" . $antwoord2.
+"<br>";
+
+
+$naam = "omesh";
+echo "welkom, $naam.";
+?>
+
+<?php
+
+
+$mijnNaam="Carl";
+$jouwNaam=Camel;
+echo $mijnNaam . " zit bij " . $jouwNaam + " in de klas!";
+?>
+
+
+
+
+
+</body>
+</html>
