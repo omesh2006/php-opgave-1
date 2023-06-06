80443ceecb3c0d5681eb611810f65cadbd12b6f0
omesh2006 <131674926+omesh2006@users.noreply.github.com>
2023-06-06 11:17:57 +0100

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,51 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Product</title>
-</head>
-<body>
-    <h2>Product Toevoegen</h2>
-    <form method="post" action="insert.php">
-
-        <label for="product_naam">Product naam:</label>
-        <input type="text" name="product_naam" required><br><br>
-
-        <label for="prijs_per_stuk">Prijs per stuk:</label>
-        <input type="number" name="prijs_per_stuk" step="0.01" required><br><br>
-
-        <label for="omschrijving">Omschrijving:</label>
-        <textarea name="omschrijving" required></textarea><br><br>
-
-        <input type="submit" value="Toevoegen">
-
-    </form>
-</body>
-</html>
-
 <?php
 
 // Verbinding maken met de database
 $host = 'localhost:3307';
-$db   = 'winkel';
+$db   = 'test';
 $user = 'root';
 $pass = '';
 $charset = 'utf8mb4';
 
-$dsn = "localhost=$host;winkel;=$db;charset=$charset";
 
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-try 
-{
-     $pdo = new PDO($dsn, $user, $pass, $options);
-     echo "Connectie gemaakt!";
-} 
-catch (\PDOException $e) 
-{
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Controleren op fouten bij het maken van de verbinding
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
->
+// De tekst weergeven
+echo "Connected to database ($dbname).";
+
+// Verbinding sluiten
+$conn->close();
+
+?>
