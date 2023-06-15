efa0aa1d3e033ab0779ca5b86b0b968e3c8e03c4
omesh2006 <131674926+omesh2006@users.noreply.github.com>
2023-06-15 13:49:53 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,44 +1,45 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>oke</title>
-</head>
-<body>
 <?php
 
-$klein = 10;
-$groot = 12;
-
-$antwoord = $klein + $groot;
-echo $klein . "+" . $groot . "=" . $antwoord.     
-"<br>";
-
-$laaggetal = 10;
-$hooggetal = 12;
-
-$antwoord2 = $laaggetal + $hooggetal;
-
-echo $laaggetal . "*" . $hooggetal . "=" . $antwoord2.
-"<br>";
+    for ($i = 1; $i <= 50; $i++) {
+        echo $i . " ";
+        }
 
 
-$naam = "omesh";
-echo "welkom, $naam.";
+        $klasgenoten = array(
+            "omesh",
+            "dylan",
+            "gino",
+            "jaden",
+            "dejah",
+            "valerio",
+            "tjay",
+            "rayan",
+            "marc",
+            "lenny"
+        );
+        
+        foreach ($klasgenoten as $naam) {
+            echo $naam . "<br>";
+        }
+        
+            $maanden = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December']; 
+            
+            echo 'Maand 1 is '.$maanden[0].'.<br />';
+            echo 'Maand 2 is '.$maanden[1].'.<br />';
+            echo 'Maand 3 is '.$maanden[2].'.<br />';
+            echo 'Maand 4 is '.$maanden[3].'.<br />';
+            echo 'Maand 5 is '.$maanden[4].'.<br />';
+            echo 'Maand 6 is '.$maanden[5].'.<br />';
+            echo 'Maand 7 is '.$maanden[6].'.<br />';
+            echo 'Maand 8 is '.$maanden[7].'.<br />';
+            echo 'Maand 9 is '.$maanden[8].'.<br />';
+            echo 'Maand 10 is '.$maanden[9].'.<br />';
+            echo 'Maand 11 is '.$maanden[10].'.<br />';
+            echo 'Maand 12 is '.$maanden[11].'.<br />';
+        
+        
+            foreach ($maanden as $index => $maand) {
+                echo 'Maand ' . ($index + 1) . ' is ' . $maand . '.<br />';
+            }
+        
 ?>
-
-<?php
-
-
-$mijnNaam="Carl";
-$jouwNaam=Camel;
-echo $mijnNaam . " zit bij " . $jouwNaam + " in de klas!";
-?>
-
-
-
-
-
-</body>
-</html>
