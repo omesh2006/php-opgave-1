ab18f59e9bd9b01d686b4e3b3f822ec34b32f505
omesh2006 <131674926+omesh2006@users.noreply.github.com>
2023-06-16 11:53:45 +0100

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,25 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product</title>
+</head>
+<body>
+    <h2>Product Toevoegen</h2>
+    <form method="post" action="insert.php">
+
+        <label for="product_naam">Product naam:</label><br>
+        <input type="text" name="product_naam" required><br>
+
+        <label for="prijs_per_stuk">Prijs per stuk:</label><br>
+        <input type="number" name="prijs_per_stuk" step="0.01" required><br>
+
+        <label for="omschrijving">Omschrijving:</label><br>
+        <textarea name="omschrijving" required></textarea><br>
+
+        <input type="submit" value="Toevoegen">
+
+    </form>
+</body>
+</html>
+
 <?php
 
-// Verbinding maken met de database
 $host = 'localhost:3307';
-$db   = 'test';
+$db   = 'winkel';
 $user = 'root';
 $pass = '';
 $charset = 'utf8mb4';
 
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Controleren op fouten bij het maken van de verbinding
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try 
+{
+     $pdo = new PDO($dsn, $user, $pass, $options);
+     echo "Connectie gemaakt!";
+} 
+catch (\PDOException $e) 
+{
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 
-// De tekst weergeven
-echo "Connected to database ($dbname).";
 
-// Verbinding sluiten
-$conn->close();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $product_naam = $_POST["product_naam"];
+    $prijs_per_stuk = $_POST["prijs_per_stuk"];
+    $omschrijving = $_POST["omschrijving"];
+
+
+    $sql = "INSERT INTO producten (product_naam, prijs_per_stuk, omschrijving) VALUES (?,?,?)";
+    
+    
+    $result=$pdo->prepare($sql);
+//$gegevensformulier = array($product_naam, $prijs_per_stuk, $omschrijving);
+    $result->execute([$product_naam, $prijs_per_stuk, $omschrijving]);
+
+}
+
+
+
 
 ?>
+
+
+
+    
